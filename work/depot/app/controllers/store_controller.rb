25506a9cccc9369d99db5c456ea909184f5e847f
EricMTodd <eterictodd@gmail.com>
2020-04-30 16:04:51 -0600

    Completed Chapter 10

--- work/depot/app/controllers/store_controller.rb
+++ work/depot/app/controllers/store_controller.rb
@@ -1,5 +1,12 @@
+# Add a new variable to the session to record how many times the user has accessed th store controller's index action.
+
 class StoreController < ApplicationController
   def index
     @products = Product.order(:title)
+    # if session[:counter].nil?
+    #   session[:counter] = 1
+    # else
+    #   session[:counter] += 1
+    # end
   end
 end
