409856d0425ca9133481048f6fafcc2f1bd568a3
EricMTodd <eterictodd@gmail.com>
2020-05-09 16:55:07 -0600

    working through chapter 14

--- work/depot/config/environments/development.rb
+++ work/depot/config/environments/development.rb
@@ -36,6 +36,18 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "ericmtodd.com",
+    authentication: "plain",
+    user_name: "eric",
+    password: "secret",
+    enables_starttls_auto: true
+  }
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
